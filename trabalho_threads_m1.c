bf4ddde70485b23b8bf31de72518fee6a9cb0b6b
Nicolas <105075612+nicolasrenaux@users.noreply.github.com>
2024-03-26 14:22:40 +0000

    Criando arquivo introdutório

---
+++ trabalho_threads_m1.c
@@ -0,0 +1,14 @@
+/*Código apenas introdutório (Commit 1 - Setando o ambiente)*/
+
+#include <stdio.h> // Biblioteca utilizada para o printf()
+#include <stdlib.h> // Alocação de memória e conversão de tipo de dados
+#include <pthread.h> // Biblioteca para criar e manipular threads
+#include <unistd.h> // Algumas funções como o usleep que será utilizado no desenvolvimento
+
+#define NUM_THREADS 3
+#define TOTAL_PER_UPDATE 1500
+int total_produtos = 0;
+
+main(){
+    return 0;
+}
